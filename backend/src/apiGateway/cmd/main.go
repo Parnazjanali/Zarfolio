dc40df56db5e48f6dc00992e44d0ef6cff7ed2f8
Pranaz Janali <janaliparnaz@gmail.com>
2025-05-24 22:44:37 +0330

    refactor(apiGateway): Refine server startup and dependency injection

--- backend/src/apiGateway/cmd/main.go
+++ backend/src/apiGateway/cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import routes "gold-api/internal/api/server"
+import (
+	"gold-api/internal/api/server"
+	"gold-api/internal/utils"
+)
 
-func main(){
-	routes.SetUpRoutes()
+func main() {
+	if err := utils.InitLogger(); err != nil {
+		panic(err)
+	}
+	defer utils.Log.Sync()
+
+	server.StartServer(":8080")
 }
--- backend/src/apiGateway/internal/api/middleware/auth.go
+++ backend/src/apiGateway/internal/api/middleware/auth.go
@@ -1,1 +1,8 @@
 package middleware
+
+import "github.com/gofiber/fiber/v2"
+
+func AuthUser(c *fiber.Ctx) error {
+
+	return c.Next()
+}
