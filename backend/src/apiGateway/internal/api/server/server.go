9d878d9596e996cac3407fd44f4cfeaa50a16c11
Pranaz Janali <janaliparnaz@gmail.com>
2025-05-24 22:44:37 +0330

    refactor(apiGateway): Refine server startup and dependency injection

---
+++ backend/src/apiGateway/internal/api/server/server.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"fmt"
+	"gold-api/internal/utils"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+func StartServer(port string) {
+	utils.InitLogger()
+
+	utils.Log.Info("Starting Fiber Api Gateway server")
+
+	app := fiber.New()
+	utils.Log.Info("Fiber app instance created")
+
+	apiGatewayBaseUrl := "http://localhost:8080"
+	utils.Log.Info("Initializing ProfileManagerClient", zap.String("url", apiGatewayBaseUrl))
+
+	fullAddr := fmt.Sprintf("0.0.0.0%s", port) // Listen on all network interfaces
+	utils.Log.Info("API Gateway is attempting to listen", zap.String("address", fullAddr))
+
+	log.Fatal(app.Listen(port)) // Th
+}
