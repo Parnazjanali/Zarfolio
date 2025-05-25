5819c660ef9481958ffd63b7acb0b38e522f33a7
Pranaz Janali <janaliparnaz@gmail.com>
2025-05-25 22:30:06 +0330

    refactor(auth): Define and update authentication models

--- backend/src/apiGateway/cmd/main.go
+++ backend/src/apiGateway/cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"gold-api/internal/api/server"
 	"gold-api/internal/utils"
+	"os"
 )
 
 func main() {
 	if err := utils.InitLogger(); err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to initialize logger: %w", err))
 	}
-	defer utils.Log.Sync()
+	defer func() {
+		if err := utils.Log.Sync(); err != nil && err.Error() != "sync /dev/stdout: invalid argument" {
+
+			fmt.Fprintf(os.Stderr, "Failed to sync logger: %v\n", err)
+		}
+	}()
 
 	server.StartServer(":8080")
 }
