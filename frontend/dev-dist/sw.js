e480856864570efc55cbc0a2cbc5624a89ab4af1
rezchez <rezach91@gmail.com>
2025-06-01 15:54:43 +0330

    بهبود صفحه لاگین

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mudt2viejv8"
+    "revision": "0.n1dno0iris"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
