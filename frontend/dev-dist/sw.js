e083ce4146a71b3407179cb4225498d16e4952f3
Your Name <your.email@example.com>
2025-07-30 12:01:08 +0330

    اضافه شدن قابلیت های جدید

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q9l3hh20kd"
+    "revision": "0.8bdjgk4o1ig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ frontend/src/pages/TransfersPage.jsx
@@ -0,0 +1,10 @@
+// frontend/src/pages/TransfersPage.jsx
+import React from 'react';
+import { Typography } from 'antd';
+const { Title } = Typography;
+
+const TransfersPage = () => (
+    <Title level={2}>صفحه مدیریت انتقال‌ها (در حال ساخت)</Title>
+);
+
+export default TransfersPage;
