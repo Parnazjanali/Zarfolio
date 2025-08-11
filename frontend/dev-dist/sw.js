cea991aa27e5d617090b9f3299d290555d5690e2
Your Name <your.email@example.com>
2025-08-11 17:55:34 +0330

    update

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,11 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.aa6f3ulvsng"
+<<<<<<< Updated upstream
+    "revision": "0.f487dch0l1g"
+=======
+    "revision": "0.tuno2niifa8"
+>>>>>>> Stashed changes
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
