5509e952fdec7b476149635199b1ac11beea0935
Your Name <your.email@example.com>
2025-08-19 14:56:32 +0330

    بهبود فرانت اند

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8olfe1l6ero"
+    "revision": "0.c3avtob6lh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
